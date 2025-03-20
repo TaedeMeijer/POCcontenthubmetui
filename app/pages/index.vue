74b57d9b5b33a8e3360d6d6d212e8a38aa3c73df
taede <taede@snakeware.nl>
2025-03-20 10:18:11 +0100

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="font-bold text-2xl text-(--ui-primary)">
+      Nuxt Content Hub UI POC
+    </h1>
+
+    <div class="flex items-center gap-2">
+      <UBadge class="p-2" label="Vet man"/>
+      
+      <UButton
+        label="Documentation"
+        icon="i-lucide-square-play"
+        to="https://ui3.nuxt.dev/getting-started/installation/nuxt"
+        target="_blank"
+      />
+
+      <UButton
+        label="GitHub"
+        color="neutral"
+        variant="outline"
+        icon="i-simple-icons-github"
+        to="https://github.com/nuxt/ui"
+        target="_blank"
+      />
+    </div>
+  </div>
+</template>
